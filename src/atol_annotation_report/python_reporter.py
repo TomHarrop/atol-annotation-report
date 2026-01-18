377b4122f05297e0c1c8bcb2ec8b175f1d709c98
Keeva <keeva.connolly@qcif.edu.au>
2026-01-19 09:51:09 +1000

    creating higher-level functions for parsing and mapping from each input file/tool

--- src/atol_annotation_report/python_reporter.py
+++ src/atol_annotation_report/python_reporter.py
@@ -10,17 +10,17 @@
 # this script simutaneously collects a subset of data for atol in atol_report.json.
 # atol_report.json contains an object which can be fed straight into the genome-note-lite pipeline.
 
-
+from atol_annotation_report import mapping_configs
 from importlib.resources import files
 from pathlib import Path
 import argparse
-import json
-import typst
-import yaml
 import csv
+import json
 import logging
 import sys
-from atol_annotation_report import mapping_configs
+import typst
+import yaml
+
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -106,7 +106,198 @@ def parse_arguments():
 
     return args
 
-# new functions for mapping
+# higher-level functions for mapping
+
+def map_metadata(metadata_arg, all_metadata):
+    ''' parse metadata input '''
+    if metadata_arg is not None:
+        all_metadata["metadata_input_provided"] = True
+        logger.info("Parsing metadata")
+        with open(metadata_arg, "rt") as f:
+            metadata_input = json.load(f)
+            for dictionary in metadata_input:
+                key = dictionary["meta_key"]
+                value = dictionary["meta_value"]
+                all_metadata[key] = value
+    else:
+        logger.info("No metadata file specified")
+        all_metadata["metadata_input_provided"] = False
+    return all_metadata
+
+def map_agat(agat_arg, all_agat_stats, gnl_stats):
+    ''' parse AGAT yaml and map to new field names '''
+    if agat_arg is not None:
+        all_agat_stats["agat_input_provided"] = True
+        logger.info("Parsing AGAT file")
+        with open(agat_arg, "rt") as f:
+            key_agat_stats = {}
+            full_agat_input = yaml.load(f, Loader=yaml.SafeLoader)
+            if "transcript" in full_agat_input:
+                transcript_stats = full_agat_input["transcript"]
+                key_agat_stats["feature_stats_calculated_for"] = (
+                    "transcripts (without isoforms)"
+                )
+                if "without_isoforms" in transcript_stats:
+                    agat_stats_input = transcript_stats["without_isoforms"]["value"]
+                elif "without_isoform" in transcript_stats:
+                    agat_stats_input = transcript_stats["without_isoform"]["value"]
+                else:
+                    logger.info(
+                        "AGAT stats for transcripts without isoform not found, looking for stats for mRNAs"
+                    )
+            elif "mrna" in full_agat_input:
+                mrna_stats = full_agat_input["mrna"]
+                key_agat_stats["feature_stats_calculated_for"] = (
+                    "mRNAs (without isoforms)"
+                )
+                if "without_isoforms" in mrna_stats:
+                    agat_stats_input = mrna_stats["without_isoforms"]["value"]
+                elif "without_isoform" in mrna_stats:
+                    agat_stats_input = mrna_stats["without_isoform"]["value"]
+                else:
+                    logger.info("AGAT stats for mRNAs without isoform stats not found")
+            else:
+                logger.warning("no transcript or mRNA stats detected in AGAT yaml file")
+            all_agat_stats.update(key_agat_stats)
+            map_stat_to_report(mapping_section=mapping_configs.key_agat_mappings, stat_input=agat_stats_input, report_output=key_agat_stats)
+            for mapping_sect in [
+                mapping_configs.key_agat_mappings,
+                mapping_configs.additional_agat_mappings,
+                mapping_configs.full_stat_agat_mappings,
+                mapping_configs.mean_stat_agat_mappings,
+                mapping_configs.median_stat_agat_mappings,
+                mapping_configs.long_short_agat_mappings,
+                mapping_configs.length_agat_mappings
+            ]:
+                map_stat_to_report(
+                    mapping_section=mapping_sect, 
+                    stat_input=agat_stats_input, 
+                    report_output=all_agat_stats)
+        gnl_stats.update(key_agat_stats)
+    else:
+        logger.info("No AGAT file specified")
+        all_agat_stats["agat_input_provided"] = False
+    agat_output = {"agat": all_agat_stats}
+    return agat_output
+
+def map_busco(busco_arg, all_busco_stats, gnl_stats):
+    ''' parse BUSCO json and map to new field names '''
+    if busco_arg is not None:
+        all_busco_stats["busco_input_provided"] = True
+        logger.info("Parsing BUSCO file")
+        with open(busco_arg, "rt") as f:
+            key_busco_stats = {}
+            all_busco_input = json.load(f)
+            map_stat_to_report(
+                mapping_section=mapping_configs.parameter_busco_mappings, 
+                stat_input=all_busco_input["parameters"], 
+                report_output=all_busco_stats
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.lineage_busco_mappings, 
+                stat_input=all_busco_input["lineage_dataset"], 
+                report_output=all_busco_stats
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.version_busco_mappings, 
+                stat_input=all_busco_input["versions"], 
+                report_output=all_busco_stats
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.result_busco_mappings, 
+                stat_input=all_busco_input["results"], 
+                report_output=all_busco_stats
+            )
+            map_one_to_many(
+                stat_input=all_busco_input["results"],
+                report_output=all_busco_stats,
+                mapping_section=mapping_configs.busco_val_mappings
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.key_busco_mappings, 
+                stat_input=all_busco_stats, 
+                report_output=key_busco_stats
+            )
+        gnl_stats.update(key_busco_stats)
+    else:
+        logger.info("No BUSCO file specified")
+        all_busco_stats["busco_input_provided"] = False
+    busco_output = {"busco": all_busco_stats}
+    return busco_output
+
+def map_omark(omark_arg, all_omark_stats, gnl_stats):
+    '''parse OMArk file and map to new field names'''
+    if omark_arg is not None:
+        all_omark_stats["omark_input_provided"] = True
+        logger.info("Parsing OMArk file")
+        with open(omark_arg, "rt") as f:
+            key_omark_stats = {}
+            key_omark_stats["detected_sp"] = []
+            key_omark_stats["contaminant_sp"] = []
+            all_omark_input = json.load(f)
+            omark_spp = all_omark_input["detected_species"]
+            map_stat_to_report(
+                mapping_section=mapping_configs.omark_info_mappings, 
+                stat_input=all_omark_input, 
+                report_output=key_omark_stats
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.omark_key_mappings, 
+                stat_input=all_omark_input["results_pcts"],
+                report_output=key_omark_stats
+            )
+            all_omark_stats.update(key_omark_stats)
+            map_stat_to_report(
+                mapping_section=mapping_configs.omark_consistency_mappings, 
+                stat_input=all_omark_input["results_pcts"],
+                report_output=all_omark_stats
+            )
+            map_stat_to_report(
+                mapping_section=mapping_configs.omark_conserved_hog_mappings, 
+                stat_input=all_omark_input["conserv_pcts"],
+                report_output=all_omark_stats
+            )
+            for spp in range(len(omark_spp)):
+                if "Clade" in (omark_spp[spp]):
+                    all_omark_stats["detected_sp"].append(omark_spp[spp])
+                elif "Potential_contaminants" in (omark_spp[spp]):
+                    all_omark_stats["contaminant_sp"].append(omark_spp[spp])
+            if all_omark_stats["detected_sp"] == []:
+                all_omark_stats["detected_sp"].append(None)
+            if all_omark_stats["contaminant_sp"] == []:
+                all_omark_stats["contaminant_sp"].append(None)
+        gnl_stats.update(key_omark_stats)
+    else:
+        logger.info("No OMArk file specified")
+        all_omark_stats["omark_input_provided"] = False
+    omark_output = {"omark": all_omark_stats}
+    return omark_output
+
+def map_annooddities(oddity_arg, all_oddities):
+    '''parse the annooddity summary file'''
+    if oddity_arg is not None:
+        all_oddities["annooddities_input_provided"] = True
+        logger.info("Parsing AnnoOddities file")
+        with open(oddity_arg, "rt") as f:
+            oddity_table = csv.reader(f, delimiter="\t")
+            next(oddity_table)  # take out the header
+            oddity_dict = {}
+            for row in oddity_table:
+                key = row[0]
+                value = int(row[1])
+                oddity_dict[key] = value
+            map_stat_to_report(
+                mapping_section=mapping_configs.oddity_mappings, 
+                stat_input=oddity_dict,
+                report_output=all_oddities
+            )
+    else:
+        logger.info("No AnnoOddities file specified")
+        all_oddities["annooddities_input_provided"] = False
+    oddity_output = {"annooddities": all_oddities}
+    return oddity_output
+
+# lower-level functions for mapping
 def map_stat_to_report(mapping_section, stat_input, report_output):
     for report_field, stat_field in mapping_section.items():
         if stat_field in stat_input:
@@ -159,205 +350,23 @@ def populate_template(template, input_data, output_path):
         logger.critical("typst compilation failed")
         raise e
 
-# Possibly break main up into sub-functions, e.g. one for the BUSCO file, one
-# for the OMArk, etc. If you repeat code, it should be a function. (not always
-# possible!)
 def main():
     args = parse_arguments()
 
     # this dictionary will contain a json "annotation" object which can be inserted into the atol genome-note-lite input.
     stats_for_gnl = {}
 
-    all_metadata = {}
-    if args.metadata_file is not None:
-        all_metadata["metadata_input_provided"] = True
-        logger.info("Parsing metadata")
-        with open(args.metadata_file, "rt") as f:
-            metadata_input = json.load(f)
-            for dictionary in metadata_input:
-                key = dictionary["meta_key"]
-                value = dictionary["meta_value"]
-                all_metadata[key] = value
-    else:
-        logger.info("No metadata file specified")
-        all_metadata["metadata_input_provided"] = False
-
-    all_agat_stats = {}
-    if args.agat_file is not None:
-        # TODO: parse and map AGAT software version and add to key_agat_mappings
-        # parse AGAT yaml and map to new field names
-        all_agat_stats["agat_input_provided"] = True
-        logger.info("Parsing AGAT file")
-        with open(args.agat_file, "rt") as f:
-            key_agat_stats = {}
-            full_agat_input = yaml.load(f, Loader=yaml.SafeLoader)
-            if "transcript" in full_agat_input:
-                transcript_stats = full_agat_input["transcript"]
-                key_agat_stats["feature_stats_calculated_for"] = (
-                    "transcripts (without isoforms)"
-                )
-                if "without_isoforms" in transcript_stats:
-                    agat_stats_input = transcript_stats["without_isoforms"]["value"]
-                elif "without_isoform" in transcript_stats:
-                    agat_stats_input = transcript_stats["without_isoform"]["value"]
-                else:
-                    logger.info(
-                        "AGAT stats for transcripts without isoform not found, looking for stats for mRNAs"
-                    )
-            elif "mrna" in full_agat_input:
-                mrna_stats = full_agat_input["mrna"]
-                key_agat_stats["feature_stats_calculated_for"] = (
-                    "mRNAs (without isoforms)"
-                )
-                if "without_isoforms" in mrna_stats:
-                    agat_stats_input = mrna_stats["without_isoforms"]["value"]
-                elif "without_isoform" in mrna_stats:
-                    agat_stats_input = mrna_stats["without_isoform"]["value"]
-                else:
-                    logger.info("AGAT stats for mRNAs without isoform stats not found")
-            else:
-                logger.warning("no transcript or mRNA stats detected in AGAT yaml file")
-            all_agat_stats.update(key_agat_stats)
-            map_stat_to_report(mapping_section=mapping_configs.key_agat_mappings, stat_input=agat_stats_input, report_output=key_agat_stats)
-            for mapping_sect in [
-                mapping_configs.key_agat_mappings,
-                mapping_configs.additional_agat_mappings,
-                mapping_configs.full_stat_agat_mappings,
-                mapping_configs.mean_stat_agat_mappings,
-                mapping_configs.median_stat_agat_mappings,
-                mapping_configs.long_short_agat_mappings,
-                mapping_configs.length_agat_mappings
-            ]:
-                map_stat_to_report(
-                    mapping_section=mapping_sect, 
-                    stat_input=agat_stats_input, 
-                    report_output=all_agat_stats)
-        stats_for_gnl.update(key_agat_stats)
-    else:
-        logger.info("No AGAT file specified")
-        all_agat_stats["agat_input_provided"] = False
-    agat_output = {"agat": all_agat_stats}
-
-    # Tool-specific parsing could be a higher-level function (i.e. it calls the
-    # lower-level mapping functions )
-    all_busco_stats = {}
-    if args.busco_file is not None:
-        all_busco_stats["busco_input_provided"] = True
-        # parse BUSCO json and map to new field names
-        logger.info("Parsing BUSCO file")
-        with open(args.busco_file, "rt") as f:
-            key_busco_stats = {}
-            all_busco_input = json.load(f)
-            map_stat_to_report(
-                mapping_section=mapping_configs.parameter_busco_mappings, 
-                stat_input=all_busco_input["parameters"], 
-                report_output=all_busco_stats
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.lineage_busco_mappings, 
-                stat_input=all_busco_input["lineage_dataset"], 
-                report_output=all_busco_stats
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.version_busco_mappings, 
-                stat_input=all_busco_input["versions"], 
-                report_output=all_busco_stats
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.result_busco_mappings, 
-                stat_input=all_busco_input["results"], 
-                report_output=all_busco_stats
-            )
-            map_one_to_many(
-                stat_input=all_busco_input["results"],
-                report_output=all_busco_stats,
-                mapping_section=mapping_configs.busco_val_mappings
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.key_busco_mappings, 
-                stat_input=all_busco_stats, 
-                report_output=key_busco_stats
-            )
-        stats_for_gnl.update(key_busco_stats)
-    else:
-        logger.info("No BUSCO file specified")
-        all_busco_stats["busco_input_provided"] = False
-    busco_output = {"busco": all_busco_stats}
-
-    all_omark_stats = {}
-    if args.omark_file is not None:
-        all_omark_stats["omark_input_provided"] = True
-        # parse OMArk file and map to new field names
-        logger.info("Parsing OMArk file")
-        with open(args.omark_file, "rt") as f:
-            key_omark_stats = {}
-            key_omark_stats["detected_sp"] = []
-            key_omark_stats["contaminant_sp"] = []
-            all_omark_input = json.load(f)
-            omark_spp = all_omark_input["detected_species"]
-            map_stat_to_report(
-                mapping_section=mapping_configs.omark_info_mappings, 
-                stat_input=all_omark_input, 
-                report_output=key_omark_stats
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.omark_key_mappings, 
-                stat_input=all_omark_input["results_pcts"],
-                report_output=key_omark_stats
-            )
-            all_omark_stats.update(key_omark_stats)
-            map_stat_to_report(
-                mapping_section=mapping_configs.omark_consistency_mappings, 
-                stat_input=all_omark_input["results_pcts"],
-                report_output=all_omark_stats
-            )
-            map_stat_to_report(
-                mapping_section=mapping_configs.omark_conserved_hog_mappings, 
-                stat_input=all_omark_input["conserv_pcts"],
-                report_output=all_omark_stats
-            )
-            for spp in range(len(omark_spp)):
-                if "Clade" in (omark_spp[spp]):
-                    all_omark_stats["detected_sp"].append(omark_spp[spp])
-                elif "Potential_contaminants" in (omark_spp[spp]):
-                    all_omark_stats["contaminant_sp"].append(omark_spp[spp])
-            if all_omark_stats["detected_sp"] == []:
-                all_omark_stats["detected_sp"].append(None)
-            if all_omark_stats["contaminant_sp"] == []:
-                all_omark_stats["contaminant_sp"].append(None)
-        stats_for_gnl.update(key_omark_stats)
-    else:
-        logger.info("No OMArk file specified")
-        all_omark_stats["omark_input_provided"] = False
-    omark_output = {"omark": all_omark_stats}
-
-    all_oddities = {}
-    if args.annooddities_file is not None:
-        all_oddities["annooddities_input_provided"] = True
-        # parse the annooddity summary file
-        logger.info("Parsing AnnoOddities file")
-        with open(args.annooddities_file, "rt") as f:
-            oddity_table = csv.reader(f, delimiter="\t")
-            next(oddity_table)  # take out the header
-            oddity_dict = {}
-            for row in oddity_table:
-                key = row[0]
-                value = int(row[1])
-                oddity_dict[key] = value
-            map_stat_to_report(
-                mapping_section=mapping_configs.oddity_mappings, 
-                stat_input=oddity_dict,
-                report_output=all_oddities
-            )
-    else:
-        logger.info("No AnnoOddities file specified")
-        all_oddities["annooddities_input_provided"] = False
-    oddity_output = {"annooddities": all_oddities}
+    # mapping input files to output dictionaries
+    processed_metadata = map_metadata(metadata_arg=args.metadata_file, all_metadata={})
+    processed_agat = map_agat(agat_arg=args.agat_file, all_agat_stats={}, gnl_stats=stats_for_gnl)
+    processed_busco = map_busco(busco_arg=args.busco_file, all_busco_stats={}, gnl_stats=stats_for_gnl)
+    processed_omark = map_omark(omark_arg=args.omark_file, all_omark_stats={}, gnl_stats=stats_for_gnl)
+    processed_annooddities = map_annooddities(oddity_arg=args.annooddities_file, all_oddities={})
 
     logger.info("Combining statistics and writing to JSON")
 
     combined_stats = (
-        all_metadata | agat_output | busco_output | omark_output | oddity_output
+        processed_metadata | processed_agat | processed_busco | processed_omark | processed_annooddities
     )
 
     # format genome note lite dictionary as 'annotation' object
